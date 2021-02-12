6076d580cbe5089dd1e02b1ca9adaaf95817386c
Leo Paz <omarleopaz@hotmail.com>
2021-02-12 12:05:12 -0500

    Trying to run tests on build

--- src/test/java/Lab4/demo/TestingWebApplicationFullTest.java
+++ src/test/java/Lab4/demo/TestingWebApplicationFullTest.java
@@ -1,21 +1,20 @@
 package Lab4.demo;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.jupiter.api.Test;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-
 @SpringBootTest
 @AutoConfigureMockMvc
-public class TestingWebApplicationTestFull {
+public class TestingWebApplicationFullTest {
 
     @Autowired
     private MockMvc mockMvc;
